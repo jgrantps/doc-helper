74764f59a911a92c4a57cb5a26c957d415f6a7ee
jgrantps <jgrantps@hotmail.com>
2019-11-07 10:06:24 -0400

    stubbed out user controller and conditional signin sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/update'
+  get 'users/edit'
   devise_for :users,  controllers: {
     confirmations: 'confirmations'
   } 
+  resources :users
   resources :dashboard, only: [:index]
   resources :home, only: [:index]
   root to: "home#index"
