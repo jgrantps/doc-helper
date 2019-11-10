23bf935b20a1c27b85bb72e2d475f8f7a392dec3
jgrantps <jgrantps@hotmail.com>
2019-11-10 09:59:55 -0400

    add admin seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  enum role: [:manager, :contact, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :contact
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DATA = {
+  :user_keys => 
+  ["name", "username", "email", "role", "password"], 
+  :users =>[
+    ["Peter Grant", "jgrantps", "jgrantps@hotmail.com", "admin", "password111"]
+  ]
+}
+
+def main
+  DATA[:users].each do |user|
+    new_user = User.new
+    user.each_with_index do |attribute, i|
+      new_user.send(DATA[:user_keys][i]+"=", attribute)
+    end
+
+    new_user.save
+  end
+end
+main
