214ea509630ba18fddba6f54e2e7b0ac107f24bc
jgrantps <jgrantps@hotmail.com>
2019-11-14 08:01:50 -0400

    add seed data users and companies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,38 @@ DATA = {
   :user_keys => 
   ["name", "username", "email", "role", "password"], 
   :users =>[
-    ["Peter Grant", "jgrantps", "jgrantps@hotmail.com", "admin", "password111"]
-  ]
+    ["Peter Grant", "jgrantps", "jgrantps@hotmail.com", "admin", "password111"], #=>1
+    ["Jerry Mitchell", "JerryM", "jerry@gmhomes.com", "contact", "password111"], #=>2
+    ["Jessesar Mitchell", "JessM", "jessesar@gmhomes.com", "contact", "password111"], #=>3
+    ["Angela Mitchell", "AngelaM", "angela@gmhomes.com", "contact", "password111"], #=>4
+    ["Conrad Mullins", "ConradM", "conrad@grain.ca", "contact", "password111"], #=>5
+    ["Melvin Tibert", "MelvinT", "mel@meandtheboys.ca", "contact", "password111"], #=>6
+    ["Mike Jones", "MikeJ", "mike@jonesco.ca", "contact", "password111"], #=>7
+    ["Fiasal Forhart", "FiasalF", "fiasal@onthelevel.ca", "contact", "password111"], #=>8
+    ["Traci Mathison", "TraciM", "traci@trinitywealth.com", "manager", "password111"], #=>9
+    ["Nancy Reynolds", "NancyR", "nancy@trinitywealth.com", "manager", "password111"], #=>10
+    ["David Lynn", "DavidL", "david@trinitywealth.com", "manager", "password111"], #=>11
+    ["Kim Fougere", "KimF", "kim@fougerebookkeeping.com", "manager", "password111"], #=>12
+    ["Dave Fougere", "DaveF", "Dave@fougerebookkeeping.com", "manager", "password111"], #=>13
+    ["Caleb Bowers", "CalebB", "caleb@bowersconstruction.ca", "contact", "password111"], #=>14
+    ["Dean Hartman", "DeanH", "dean@borisdevelopments.ca", "contact", "password111"], #=>15
+  ],
+  :company_keys =>
+  ["name"],
+  :companies =>[
+    ["Gerald Mitchell Contracting"], #=> 
+    ["Mercers Best Built Structures"], #=> 
+    ["Grain Spaces"], #=> 
+    ["On The Level"], #=> 
+    ["Me And The Boys Contracting"], #=> 
+    ["JonesCo Contracting"], #=> 
+    ["Bowers Construction"], #=> 
+    ["Boris Developments"], #=> 
+    
+  ],
+  :account_keys =>
+
+
 }
 
 def main
