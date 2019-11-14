af32760eae369b4bcd79884173296546709181c9
jgrantps <jgrantps@hotmail.com>
2019-11-14 07:13:09 -0400

    first commit for nov14

--- db/migrate/20191113160333_add_company_ref_to_users_companies.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyRefToUsersCompanies < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :users_companies, :company, null: false, foreign_key: true
-  end
-end
--- db/migrate/20191113160346_add_user_ref_to_users_companies.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToUsersCompanies < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :users_companies, :user, null: false, foreign_key: true
-  end
-end
