8ec613a7b1d99862f1171031e6349e40c750d651
jgrantps <jgrantps@hotmail.com>
2019-11-25 12:32:42 -0400

    broken link_to in company user_list

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,15 @@ Rails.application.routes.draw do
     get  "/contacts", to: "users#contacts"
     resources :companies
   end
+  
+  resources :companies do
+    get  "users/all", to: "users#all"
+    get  "users/managers", to: "users#managers"
+    get  "users/admins", to: "users#admins"
+    get  "users/contacts", to: "users#contacts"
+  end
+
+
   resources :accounts
   resources :packages
   resources :documents
